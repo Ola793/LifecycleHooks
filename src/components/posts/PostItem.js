0f63f5852bdbf3976dd39c7d4d3fd64cf383d94d
Ola793 <67291449+Ola793@users.noreply.github.com>
2022-04-22 12:35:14 +0300

    Update PostItem.js

--- src/components/posts/PostItem.js
+++ src/components/posts/PostItem.js
@@ -2,10 +2,6 @@ import React from "react";
 import "../../App.css";
 
 export default class PostItem extends React.Component {
-    // eslint-disable-next-line no-useless-constructor
-    constructor(props) {
-        super(props);
-    }
     render() {
         return <div className="post-title">{this.props.title}</div>;
     }
